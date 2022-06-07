334b616e5ac85ebb91f16d93085ad58485bb030a
crewskt <106830985+crewskt@users.noreply.github.com>
2022-06-07 14:32:55 -0300

    1

--- src/components/pages/ajuda/quemsomos.js
+++ src/components/pages/ajuda/quemsomos.js
@@ -13,7 +13,7 @@ const Ajuda = () => (
         <div className="box-description">
 
             <div className="box-title">
-                <h1 class="title-ajuda"> Projeto integrador  </h1>
+                <h1 class="title-ajuda"> Projeto integrado  </h1>
             </div>
 
             <div className="box-mente">
