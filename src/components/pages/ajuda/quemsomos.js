318bfd6ba11d8b46b6259382c2472b0a0f5b1d04
Raul <106830985+crewskt@users.noreply.github.com>
2022-09-15 18:52:57 -0300

    1

--- src/components/pages/ajuda/quemsomos.js
+++ src/components/pages/ajuda/quemsomos.js
@@ -12,34 +12,6 @@ const Ajuda = () => (
 
         <div className="box-description">
 
-            <div className="box-title">
-                <h1 class="title-ajuda"> Projeto integrado  </h1>
-            </div>
-
-            <div className="box-mente">
-                <p className="description"> 
-            Técnicos em enfermagem Turma de 2021.1 no Senac RJ Madureira</p>
-            <p></p>
-            <p></p>
-            <div>
-            <p></p><h4>EQUIPE </h4>
-            </div>
-            <p></p>
-            <p></p>
-            <p></p><h6> João Felipe  </h6>
-             <p></p>
-             <p>Bruna Moreira</p>
-             <p></p>
-             <p>Rafaelle Soares</p>
-             <p></p>
-             <p>Diana Calçada</p>
-             <p></p>
-             <p>Stephany Rocha</p>
-             <p></p>
-             <p>Wilzete Silva</p>
-             <p></p>
-            </div> <p></p>
-##########################################################################
          <h2 class="infor3">Técnicos em enfermagem Turma de 2021.1 no Senac RJ Madureira</h2>
          </div>
            <div className="row-lista-img">
