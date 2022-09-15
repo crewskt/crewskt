fcf432ce2d186f0cb2b733d048d71f2d8f44bd51
Raul <106830985+crewskt@users.noreply.github.com>
2022-09-15 19:00:47 -0300

    1

--- src/components/pages/ajuda/quemsomos.js
+++ src/components/pages/ajuda/quemsomos.js
@@ -19,21 +19,21 @@ const Ajuda = () => (
          <li className="li-sintomas"></li>
            
              <ul className="ul-sintomas">
-             <li className="li-sintomas"></li>
+             
                <li className="li-sintomas">João Felipe</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Bruna Moreira</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Alterações de sono;</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Rafaelle Soares</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Diana Calçada</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Stephany Rocha</li>
-               <li className="li-sintomas"></li>
+               
                <li className="li-sintomas">Wilzete Silva</li>
-               <li className="li-sintomas"></li>
+               
              </ul>
 
 
